22012717f6619e14362ae29fd5840a1868565e12
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-07-03 12:43:33 +0300

    add aliases in zsh configuration

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,19 @@
+# Git shortcuts
+alias gst='git status'
+alias ga='git add'
+alias gc='git commit -m'
+alias gp='git push'
+alias gpuom='git push -u origin main'
+alias gmain='git branch -m main'
+alias glog='git log --oneline --graph --decorate --all --date=relative'
+alias gundo='git reset --soft HEAD^'
+
+# General navigation
+
+# Repo navigation
+alias repos='cd $HOME/repos'
+alias dots='repos && cd dotfiles'
+
+# LS with colours
+alias ll='ls -lG'   # list long, human-readable, classify
+alias la='ls -lAG'  # include hidden
--- .zshrc
+++ .zshrc
@@ -2,3 +2,6 @@ if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
   eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/default_config.toml)"
 fi
 
+if [ -f ~/.zsh/aliases.zsh ]; then
+  source ~/.zsh/aliases.zsh
+fi
