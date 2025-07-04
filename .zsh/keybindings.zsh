a42ab951c89cc8ee8c6b5bd9182cf66b07a8a7a6
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-07-04 10:14:03 +0300

    remove autosuggestions plugin

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -1,7 +1,4 @@
 # Bind custom keys
-bindkey '^[[9674;7u' autosuggest-accept
-bindkey '^[[8710;7u' autosuggest-clear
-
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
