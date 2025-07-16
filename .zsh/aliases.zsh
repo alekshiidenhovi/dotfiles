71499bf8a4bbc4978e47bf4b05ccf58203028278
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-07-16 14:34:15 +0300

    re-implement gundo alias to function

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,8 +4,21 @@ alias gp='git push'
 alias gpuom='git push -u origin main'
 alias gmain='git branch -m main'
 alias glog='git log --oneline --graph --decorate --all --date=relative'
-alias gundo='git reset --soft HEAD^'
 alias gamend='git commit --no-edit --amend'
+
+gundo() {
+    if [ -z "$1" ]; then
+        echo "No number of commits specified. Undoing the last 1 commit (--soft)."
+        git reset --soft HEAD~1
+    elif ! [[ "$1" =~ ^[1-9]+$ ]]; then
+        echo "Error: Argument must be a positive integer."
+        echo "Usage: gundo <number_of_commits>"
+        return 1
+    else
+        echo "Undoing the last $1 commit(s) (--soft)."
+        git reset --soft "HEAD~$1"
+    fi
+}
 
 # General navigation
 alias fvim='nvim $(fzf -m --preview="bat --color=always {}")'
