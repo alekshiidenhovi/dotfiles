daaab3044c997f5bc1d2d6ff0e0863601819aed1
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-15 15:42:52 +0300

    add keymap support for git and lsp in telescope

--- .config/nvim/lua/plugins/lazy/telescope.lua
+++ .config/nvim/lua/plugins/lazy/telescope.lua
@@ -22,10 +22,21 @@ return {
       local builtin = require('telescope.builtin')
 
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>frg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set("n", "<leader>en", function()
+      vim.keymap.set('n', '<leader>fm', builtin.man_pages, { desc = 'Telescope manual pages' })
+
+      vim.keymap.set('n', '<leader>fgc', builtin.git_commits, { desc = 'Telescope git commits' })
+      vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
+      vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git statuses' })
+
+      vim.keymap.set('n', '<leader>flr', builtin.lsp_references, { desc = 'Telescope LSP references' })
+      vim.keymap.set('n', '<leader>fli', builtin.lsp_implementations, { desc = 'Telescope LSP implementations' })
+      vim.keymap.set('n', '<leader>fld', builtin.lsp_definitions, { desc = 'Telescope LSP definitions' })
+
+
+      vim.keymap.set("n", "<leader>fc", function()
         builtin.find_files {
           cwd = vim.fn.stdpath("config")
         }
