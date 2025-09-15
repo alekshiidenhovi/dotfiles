54536a1b3981c648544b33f4e22bf18c6fe21731
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-15 09:38:41 +0300

    add support for more telescope commands

--- .config/nvim/lua/plugins/lazy/telescope.lua
+++ .config/nvim/lua/plugins/lazy/telescope.lua
@@ -19,11 +19,14 @@ return {
       }
 
       require("telescope").load_extension("fzf")
+      local builtin = require('telescope.builtin')
 
-      vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
-      vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<space>en", function()
-        require("telescope.builtin").find_files {
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files {
           cwd = vim.fn.stdpath("config")
         }
       end)
