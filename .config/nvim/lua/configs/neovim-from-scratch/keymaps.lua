de655dcc6967c8e3ec173b7008125fe07cce7915
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-15 20:55:24 +0300

    add keymaps for quickfixes

--- .config/nvim/lua/configs/neovim-from-scratch/keymaps.lua
+++ .config/nvim/lua/configs/neovim-from-scratch/keymaps.lua
@@ -46,6 +46,10 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
+-- Quickfix
+vim.keymap.set("n", "<A-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<A-k>", "<cmd>cprev<CR>")
+
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
