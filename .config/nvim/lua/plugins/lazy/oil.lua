6c72ed02cf65cb08ba3736dca5b00f20b1a40af8
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-16 13:47:53 +0300

    use nvim webdev icons instead for oil

--- .config/nvim/lua/plugins/lazy/oil.lua
+++ .config/nvim/lua/plugins/lazy/oil.lua
@@ -4,7 +4,7 @@ return {
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
   }
