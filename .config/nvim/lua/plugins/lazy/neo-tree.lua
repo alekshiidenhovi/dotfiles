6ae37977a6efdb6bf04aab032dd5302ea46ed50e
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-16 14:04:06 +0300

    add neotree plugin with basic config

---
+++ .config/nvim/lua/plugins/lazy/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false,                    -- neo-tree will lazily load itself
+    ---@module 'neo-tree'
+    ---@type neotree.Config
+    opts = {
+      enable_git_status = true,
+      enable_diagnostics = true,
+      close_if_last_window = true,
+    }
+  }
+}
