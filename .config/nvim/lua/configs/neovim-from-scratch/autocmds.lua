8909e9378e15b04090bd2146454ddb6ceacbe7d6
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-16 09:06:16 +0300

    add basic autocmds for highlight yanking and terminal opening

---
+++ .config/nvim/lua/configs/neovim-from-scratch/autocmds.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  desc = "Hide number lines when in terminal mode",
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end
+})
