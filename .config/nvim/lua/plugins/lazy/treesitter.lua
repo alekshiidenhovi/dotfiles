dcb1ac4dfacfb262b3901d5d07176758b0d41b4a
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-16 14:03:52 +0300

    format treesitter plugin config

--- .config/nvim/lua/plugins/lazy/treesitter.lua
+++ .config/nvim/lua/plugins/lazy/treesitter.lua
@@ -37,16 +37,16 @@ return {
           enable = true,
           -- Disable ighlight for large files
           disable = function(lang, buf)
-              local max_filesize = 100 * 1024 -- 100 KB
-              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-              if ok and stats and stats.size > max_filesize then
-                  return true
-              end
+            local max_filesize = 100 * 1024   -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
           end,
           -- Disable legacy VimScript regex-based highlighting
           additional_vim_regex_highlighting = false,
-         },
-       }
+        },
+      }
     end,
   }
 }
