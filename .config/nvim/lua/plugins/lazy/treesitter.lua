d28d65f323b24bd61f193d3936f60b9f662294ff
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-19 10:55:06 +0300

    add missing fields to treesitter config

--- .config/nvim/lua/plugins/lazy/treesitter.lua
+++ .config/nvim/lua/plugins/lazy/treesitter.lua
@@ -32,12 +32,14 @@ return {
         },
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        sync_install = false,
         auto_install = false,
+        ignore_install = {},
         highlight = {
           enable = true,
           -- Disable ighlight for large files
           disable = function(lang, buf)
-            local max_filesize = 100 * 1024   -- 100 KB
+            local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
               return true
