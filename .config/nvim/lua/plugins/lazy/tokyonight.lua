c6484ceeedefe289c83d9b8e6f4e953fd5150484
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-23 10:20:04 +0300

    set linenumber colors for tokyonight

--- .config/nvim/lua/plugins/lazy/tokyonight.lua
+++ .config/nvim/lua/plugins/lazy/tokyonight.lua
@@ -2,6 +2,17 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  config = function() vim.cmd.colorscheme "tokyonight" end,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+      on_highlights = function(hl, c)
+        hl.LineNr = { fg = c.blue }
+        hl.LineNrAbove = { fg = c.blue }
+        hl.LineNrBelow = { fg = c.blue }
+        hl.CursorLineNr = { fg = c.orange, bold = true }
+      end,
+    })
+    vim.cmd.colorscheme "tokyonight"
+  end,
   enabled = true
 }
