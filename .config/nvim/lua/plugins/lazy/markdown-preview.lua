f4f8af36dc81f49d7a657994ccf46167b7854220
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-11-05 11:54:19 +0200

    integrate markview with codecompanion

--- .config/nvim/lua/plugins/lazy/markdown-preview.lua
+++ .config/nvim/lua/plugins/lazy/markdown-preview.lua
@@ -5,12 +5,41 @@ return {
     dependencies = {
       "saghen/blink.cmp"
     },
-    preview = {
-      icon_provider = "devicons"
-    },
+    ft = { "markdown", "codecompanion" },
     config = function()
+      require("markview").setup({
+        preview = {
+          icon_provider = "devicons",
+          filetypes = {
+            "md",
+            "markdown",
+            "latex",
+            "vimwiki",
+            "typst",
+            "codecompanion",
+          },
+          ignore_buftypes = {},
+          condition = function(buffer)
+            local ft, bt = vim.bo[buffer].ft, vim.bo[buffer].bt;
+
+            if bt == "nofile" and ft == "codecompanion" then
+              return true;
+            elseif bt == "nofile" then
+              return false;
+            else
+              return true;
+            end
+          end
+        },
+      })
+
       local keymap = vim.keymap.set
       keymap("n", "<leader>m", "<cmd>Markview toggle<CR>", { silent = true, desc = "Toggle Preview" })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'codecompanion',
+        command = 'Markview attach',
+      })
     end
   }
 };
