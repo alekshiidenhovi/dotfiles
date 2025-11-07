108cf48ba4ea2c1700ccf9d30c547a6799388e90
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-11-07 10:55:16 +0200

    reorder which key groups alphabetically

--- .config/nvim/lua/plugins/lazy/which-key.lua
+++ .config/nvim/lua/plugins/lazy/which-key.lua
@@ -7,15 +7,15 @@ return {
       spec = {
         { "<C-t>", group = "terminal", icon = "" },
         { "<leader>a", group = "ai", icon = "󱜙" },
+        { "<leader>b", group = "buffer", icon = "󰈔" },
         { "<leader>c", group = "clipboard", icon = "" },
-        { "<leader>x", group = "execute", icon = "" },
         { "<leader>f", group = "find", icon = "󰈞" },
         { "<leader>g", group = "git", icon = "" },
         { "<leader>l", group = "lsp", icon = "" },
-        { "<leader>s", group = "supermaven", icon = "" },
-        { "<leader>b", group = "buffer", icon = "󰈔" },
-        { "<leader>z", group = "Zen Mode", icon = "󱅻" },
         { "<leader>L", group = "Lazy", icon = "󰒲" },
+        { "<leader>s", group = "supermaven", icon = "" },
+        { "<leader>x", group = "execute", icon = "" },
+        { "<leader>z", group = "Zen Mode", icon = "󱅻" },
       }
     },
     keys = {
