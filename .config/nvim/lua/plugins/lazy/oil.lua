e4db8da4f7f5aa287fcc76c7a4d0bef4c786bd71
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-01-12 10:08:25 +0200

    setup allowed hidden files for oil

--- .config/nvim/lua/plugins/lazy/oil.lua
+++ .config/nvim/lua/plugins/lazy/oil.lua
@@ -5,10 +5,35 @@ return {
     ---@type oil.SetupOpts
     dependencies = { "nvim-tree/nvim-web-devicons" },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    opts = {},
+    opts = {
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          if name:match("^%node_modules") then
+            return true
+          end
+
+          local match = name:match("^%.")
+          if match == nil then
+            return false
+          end
+
+          if name:match("^%.gitignore") then
+            return false
+          end
+
+          if name:match("^%.env") then
+            return false
+          end
+
+          return true
+        end
+      }
+    },
     keys = {
       { "-", "<cmd>Oil<CR>", desc = "Open parent directory", mode = "n" }
     },
     lazy = false,
+
   }
 }
