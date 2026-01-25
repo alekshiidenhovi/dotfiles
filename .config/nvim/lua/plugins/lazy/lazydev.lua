94ea359d8be88034648514aa3aa081ba39c50a49
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-01-25 21:29:39 +0200

    setup lazydev.nvim

---
+++ .config/nvim/lua/plugins/lazy/lazydev.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { -- optional blink completion source for require statements and module annotations
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  }
+}
--- .config/nvim/lua/plugins/lazy/lsp.lua
+++ .config/nvim/lua/plugins/lazy/lsp.lua
@@ -42,17 +42,7 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       'saghen/blink.cmp',
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
+      'folke/lazydev.nvim',
     },
     opts = {
       servers = {
